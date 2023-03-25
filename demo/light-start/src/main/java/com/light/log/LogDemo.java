517d933e9ace8d4ceb68120be0e5cf6f7240398c
light <loversgzlqian@gmail.com>
2023-03-25 10:31:00 +0800

    log

--- demo/light-start/src/main/java/com/light/log/LogDemo.java
+++ demo/light-start/src/main/java/com/light/log/LogDemo.java
@@ -9,12 +9,12 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 @Component
-//@Slf4j
+@Slf4j
 public class LogDemo {
     //直接加注解可以不写下面这句
-    private static final Logger log = LoggerFactory.getLogger("类名");
+//    private static final Logger log = LoggerFactory.getLogger("类名");
 
     public static void main(String[] args) {
-
+        log.info("xx");
     }
 }
