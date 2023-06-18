a897452d2408d31ed9ce82e918c023e92c2c4dc9
light <loversgzlqian@gmail.com>
2023-06-18 12:01:35 +0800

    RabbitMQ实践-消费者

--- demo/light-mq/src/main/java/com/light/mq/RabbitMQService.java
+++ demo/light-mq/src/main/java/com/light/mq/RabbitMQService.java
@@ -1,6 +1,8 @@
 package com.light.mq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +14,17 @@ import javax.annotation.Resource;
  * @Date 2023/6/17
  * @Desc
  **/
+@Slf4j
 @Service
 public class RabbitMQService {
 
     @Resource
     private RabbitAdmin rabbitAdmin;
 
+    /**
+     * 创建三种交换机，将不同的交换机绑定到不同的队列中
+     * 可以直连，交换机绑定队列；也可以流转，交换机绑定交换机，再转到队列中
+     * */
     public void sendMsg() {
         /*
          * 三种交换机，
@@ -29,9 +36,9 @@ public class RabbitMQService {
         rabbitAdmin.declareExchange(new FanoutExchange("test.fanout.exchange", false, false, null));
         rabbitAdmin.declareExchange(new TopicExchange("test.topic.exchange", false, false, null));
 
-        rabbitAdmin.declareQueue(new Queue("topic.queue", false, false, false, null));
-        rabbitAdmin.declareQueue(new Queue("direct.queue", false, false, false, null));
         rabbitAdmin.declareQueue(new Queue("fanout.queue", false, false, false, null));
+        rabbitAdmin.declareQueue(new Queue("direct.queue", false, false, false, null));
+        rabbitAdmin.declareQueue(new Queue("topic.queue", false, false, false, null));
 
         //1将一个queue绑定到一个exchange
         rabbitAdmin.declareBinding(new Binding("direct.queue",//目标：队列名
@@ -60,4 +67,15 @@ public class RabbitMQService {
         rabbitAdmin.getRabbitTemplate().convertAndSend("test.topic.exchange", "test", "多级流转消息2222");
 
     }
+
+    /**
+     * 当确认模式设置为NONE时，只要中间件投递了消息就认为成功并将消息从队列中移除。
+     * */
+    @RabbitListener(queues = "direct.queue", ackMode = "NONE")
+    public void noneAckListener(String msg) {
+        log.info("收到消息 -> {}", msg);
+        // 添加个错误用于测试
+        int a = 1 / 0;
+    }
+
 }
