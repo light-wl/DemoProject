d4655d8acabcfd1ce2e7e8277226aa73eebc0f90
light <loversgzlqian@gmail.com>
2023-06-28 15:30:57 +0800

    设计模式

--- demo/light-controller/src/main/java/com/light/designpattern/behavior/IteratorDesignPattern.java
+++ demo/light-controller/src/main/java/com/light/designpattern/behavior/IteratorDesignPattern.java
@@ -12,7 +12,9 @@ import java.util.List;
  * 实例：遍历集合数据有三种方法：for 循环、foreach 循环、iterator 迭代器
  * （foreach 循环只是一个语法糖而已，底层是基于迭代器来实现的）
  *
+ * iterator 迭代器优点：分离了集合对象的遍历行为; 抽象出了迭代器负责集合对象的遍历, 可以让外部的代码透明的访问集合内部的数据;
  * 问：在 Java 中，如果在使用迭代器的同时删除容器中的元素，会导致迭代器报错，这是为什么呢？如何来解决这个问题呢？
+ * A:不要使用集合的remove，使用 iterator.remove();即可；因为集合的remove会导致modCount次数增加，而集合在迭代时不允许修改。
  *
  **/
 public class IteratorDesignPattern {
@@ -23,7 +25,12 @@ public class IteratorDesignPattern {
         names.add("zheng");
         Iterator<String> iterator = names.iterator();
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            // 会报错：names.remove(iterator.next());
+            String str = iterator.next();
+            if("xzg".equals(str)){
+                iterator.remove();
+            }
+            System.out.println(str);
         }
 
         for(String name : names){
@@ -31,7 +38,6 @@ public class IteratorDesignPattern {
         }
     }
 }
-
 
 class ArrayIterator<E> implements Iterator<E> {
     private int cursor;
--- demo/light-controller/src/main/java/com/light/designpattern/behavior/creates/FactoryDesignPattern.java
+++ demo/light-controller/src/main/java/com/light/designpattern/behavior/creates/FactoryDesignPattern.java
@@ -6,12 +6,17 @@ import java.util.Map;
 /**
  * @Author light
  * @Date 2023/6/25
- * @Desc 工厂模式
+ * @Desc 工厂模式 分为三类
  * 1、简单工厂，每次都需要new一个新对象
- * 2、单例 + 工厂
+ * 2、工厂方法：单例 + 工厂
+ * 3、抽象工厂
  **/
+public class FactoryDesignPattern {
+}
 
-
+/**
+ * 简单工厂实现一
+ */
 class RuleConfigParserFactoryOne {
     public static IRuleConfigParser createParser(String configFormat) {
         IRuleConfigParser parser = null;
@@ -28,42 +33,118 @@ class RuleConfigParserFactoryOne {
     }
 }
 
-
+/**
+ * 简单工厂实现二
+ */
 class RuleConfigParserFactoryTwo {
-  private static final Map<String, IRuleConfigParser> cachedParsers = new HashMap<>();
+    private static final Map<String, IRuleConfigParser> cachedParsers = new HashMap<>();
 
-  static {
-    cachedParsers.put("json", new JsonRuleConfigParser());
-    cachedParsers.put("xml", new XmlRuleConfigParser());
-    cachedParsers.put("yaml", new YamlRuleConfigParser());
-    cachedParsers.put("properties", new PropertiesRuleConfigParser());
-  }
-
-  public static IRuleConfigParser createParser(String configFormat) {
-    if (configFormat == null || configFormat.isEmpty()) {
-      return null;//返回null还是IllegalArgumentException全凭你自己说了算
+    static {
+        cachedParsers.put("json", new JsonRuleConfigParser());
+        cachedParsers.put("xml", new XmlRuleConfigParser());
+        cachedParsers.put("yaml", new YamlRuleConfigParser());
+        cachedParsers.put("properties", new PropertiesRuleConfigParser());
     }
-    IRuleConfigParser parser = cachedParsers.get(configFormat.toLowerCase());
-    return parser;
-  }
+
+    public static IRuleConfigParser createParser(String configFormat) {
+        if (configFormat == null || configFormat.isEmpty()) {
+            return null;//返回null还是IllegalArgumentException全凭你自己说了算
+        }
+        IRuleConfigParser parser = cachedParsers.get(configFormat.toLowerCase());
+        return parser;
+    }
 }
 
-/**配置参数*/
-public class FactoryDesignPattern {
+/**
+ * 工厂方法
+ */
+class RuleConfigSource {
+    public IRuleConfigParser load(String ruleConfigFilePath) throws Exception {
+        String ruleConfigFileExtension = getFileExtension(ruleConfigFilePath);
+
+        IRuleConfigParserFactory parserFactory = null;
+        if ("json".equalsIgnoreCase(ruleConfigFileExtension)) {
+            parserFactory = new JsonRuleConfigParserFactory();
+        } else if ("xml".equalsIgnoreCase(ruleConfigFileExtension)) {
+            parserFactory = new XmlRuleConfigParserFactory();
+        } else if ("yaml".equalsIgnoreCase(ruleConfigFileExtension)) {
+            parserFactory = new YamlRuleConfigParserFactory();
+        } else if ("properties".equalsIgnoreCase(ruleConfigFileExtension)) {
+            parserFactory = new PropertiesRuleConfigParserFactory();
+        } else {
+            throw new Exception("Rule config file format is not supported: " + ruleConfigFilePath);
+        }
+        IRuleConfigParser parser = parserFactory.createParser();
+
+        String configText = "";
+        //从ruleConfigFilePath文件中读取配置文本到configText中
+        parser.parse(configText);
+        IRuleConfigParser ruleConfig = null;
+        return ruleConfig;
+    }
+
+    private String getFileExtension(String filePath) {
+        //...解析文件名获取扩展名，比如rule.json，返回json
+        return "json";
+    }
 }
 
+interface IRuleConfigParserFactory {
+    IRuleConfigParser createParser();
+}
+
+class JsonRuleConfigParserFactory implements IRuleConfigParserFactory {
+    @Override
+    public IRuleConfigParser createParser() {
+        return new JsonRuleConfigParser();
+    }
+}
+
+class XmlRuleConfigParserFactory implements IRuleConfigParserFactory {
+    @Override
+    public IRuleConfigParser createParser() {
+        return new XmlRuleConfigParser();
+    }
+}
+
+class YamlRuleConfigParserFactory implements IRuleConfigParserFactory {
+    @Override
+    public IRuleConfigParser createParser() {
+        return new YamlRuleConfigParser();
+    }
+}
+
+class PropertiesRuleConfigParserFactory implements IRuleConfigParserFactory {
+    @Override
+    public IRuleConfigParser createParser() {
+        return new PropertiesRuleConfigParser();
+    }
+}
 
 interface IRuleConfigParser {
+    void parse(String text);
 }
 
 class JsonRuleConfigParser implements IRuleConfigParser {
+    @Override
+    public void parse(String text) {
+    }
 }
 
 class XmlRuleConfigParser implements IRuleConfigParser {
+    @Override
+    public void parse(String text) {
+    }
 }
 
 class YamlRuleConfigParser implements IRuleConfigParser {
+    @Override
+    public void parse(String text) {
+    }
 }
 
 class PropertiesRuleConfigParser implements IRuleConfigParser {
+    @Override
+    public void parse(String text) {
+    }
 }
