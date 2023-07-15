1ac33bcb57bae6e7db72928987333998f695f359
light <loversgzlqian@gmail.com>
2023-07-15 15:54:44 +0800

    test

--- demo/light-dubbo/src/main/java/com/light/Main.java
+++ demo/light-dubbo/src/main/java/com/light/Main.java
@@ -1,16 +1,12 @@
 package com.light;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * @Description
  * @Author ${USER}
- * @Date ${DATE} ${TIME}
+ * @Date ${DATE}
+ * @Desc
  **/
 public class Main {
     public static void main(String[] args) {
-        List<String> temp = new ArrayList<>();
         System.out.println("Hello world!");
     }
 }
