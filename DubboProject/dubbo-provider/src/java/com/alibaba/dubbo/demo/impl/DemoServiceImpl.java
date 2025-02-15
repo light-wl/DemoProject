de62ba6dc899ae732690e37e3a771b2070427a5a
loversgzl <1270762753@qq.com>
2025-02-16 02:28:46 +0800

    1

--- DubboProject/dubbo-provider/src/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
+++ DubboProject/dubbo-provider/src/java/com/alibaba/dubbo/demo/impl/DemoServiceImpl.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 public class DemoServiceImpl implements DemoService {
     public List<String> getPermissions(Long id) {
+        System.out.println("getPermissions调用开始");
         List<String> demo = new ArrayList<String>();
         demo.add(String.format("Permission_%d", id - 1));
         demo.add(String.format("Permission_%d", id));
         demo.add(String.format("Permission_%d", id + 1));
+        System.out.println("getPermissions调用结束");
         return demo;
     }
 }
